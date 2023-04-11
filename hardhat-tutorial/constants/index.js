3cb9edd839395804e45f9e86954a1db34612ee1f
Sigit Haryoyudanto <83746683+vvarl0cks@users.noreply.github.com>
2023-04-11 16:35:33 +0800

    first commit
    hardhat

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0x52fc9729586A1dd5d3c16326313b034040379296";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://raw.githubusercontent.com/LearnWeb3DAO/NFT-Collection/main/my-app/public/cryptodevs/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
